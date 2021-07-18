9b8b66f29639dc10eab4c1c1d941cce4f6faa77d
romanxeo <romanxeo@gmail.com>
2021-07-18 17:40:37 +0300

    Counter 2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './App.module.css';
 import Counter from "./Counter";
 import Button from "./Button";
@@ -6,24 +6,44 @@ import Inputer from "./Inputer";
 
 function App() {
 
-    let countM = 6;
-    let countS = 0;
-    let countC = 0;
+    const countS = 0;
+    const countM = 5;
+    const countC = 0;
 
-    let [StartCallback, setStartCallback] = useState<number>(countS);
-    let [MaxCallback, setMaxCallback] = useState<number>(countM);
     let [countStart, setCountStart] = useState<number>(countS);
     let [countMax, setCountMax] = useState<number>(countM);
     let [countCurrent, setCountCurrent] = useState<number>(countC);
 
+    let [StartCallback, setStartCallback] = useState<number>(countS);
+    let [MaxCallback, setMaxCallback] = useState<number>(countM);
+
+
+    useEffect(() => {
+            let stringValueCountStart = localStorage.getItem('countStart')
+            if (stringValueCountStart) {
+                let numberValueCountStart = JSON.parse(stringValueCountStart)
+                setCountStart(numberValueCountStart)
+                setStartCallback(numberValueCountStart)
+            }
+
+            let stringValueCountMax = localStorage.getItem('countMax')
+            if (stringValueCountMax) {
+                let numberValueCountMax = JSON.parse(stringValueCountMax)
+                setCountMax(numberValueCountMax)
+                setMaxCallback(numberValueCountMax)
+            }
+        }, []
+    )
+
     const CountStartCallback = (vals: number) => {
         setStartCallback(vals)
-    }
 
+    }
     const CountMaxCallback = (valm: number) => {
         setMaxCallback(valm)
     }
 
+    //выставляем данные в основном стейте
     const setCountAll = () => {
         setCountStart(StartCallback)
         setCountMax(MaxCallback)
@@ -31,6 +51,9 @@ function App() {
             setCountCurrent(StartCallback)
         }
 
+        //созранеям в локал стораж
+        localStorage.setItem('countStart', JSON.stringify(StartCallback))
+        localStorage.setItem('countMax', JSON.stringify(MaxCallback))
     }
 
     const addCount = () => {
@@ -41,40 +64,41 @@ function App() {
     }
 
     const reset = () => {
-        let newCount = countStart;
-        setCountCurrent(newCount);
+        setCountCurrent(countStart);
     }
 
     return (
         <body className={s.App}>
-            <div className={s.Main}>
-                <Inputer countS={countStart}
-                         countM={countMax}
-                         CountStartCallback={CountStartCallback}
-                         CountMaxCallback={CountMaxCallback}/>
 
-                <Button name={'SET'}
-                        callback={setCountAll}
-                        countCurrent={countCurrent}
-                        countStart={countStart}
-                        countMax={countMax}/>
-            </div>
-            <div className={s.Main}>
-                <Counter countCurrent={countCurrent}
-                         countMax={countMax}/>
+        <div className={s.Main}>
+            <Inputer StartCallback={StartCallback}
+                     MaxCallback={MaxCallback}
+                     CountStartCallback={CountStartCallback}
+                     CountMaxCallback={CountMaxCallback}/>
 
-                <Button name={'ADD'}
-                        callback={addCount}
-                        countCurrent={countCurrent}
-                        countStart={countStart}
-                        countMax={countMax}/>
+            <Button name={'SET'}
+                    callback={setCountAll}
+                    countCurrent={countCurrent}
+                    countStart={countStart}
+                    countMax={countMax}/>
+        </div>
 
-                <Button name={'RESET'}
-                        callback={reset}
-                        countCurrent={countCurrent}
-                        countStart={countStart}
-                        countMax={countMax}/>
-            </div>
+        <div className={s.Main}>
+            <Counter countCurrent={countCurrent}
+                     countMax={countMax}/>
+
+            <Button name={'ADD'}
+                    callback={addCount}
+                    countCurrent={countCurrent}
+                    countStart={countStart}
+                    countMax={countMax}/>
+
+            <Button name={'RESET'}
+                    callback={reset}
+                    countCurrent={countCurrent}
+                    countStart={countStart}
+                    countMax={countMax}/>
+        </div>
         </body>
     );
 }
--- src/Inputer.tsx
+++ src/Inputer.tsx
@@ -2,35 +2,29 @@ import React, {ChangeEvent, useState} from 'react';
 import s from './App.module.css';
 
 type PropsType = {
-    countS: number
-    countM: number
+    StartCallback: number
+    MaxCallback: number
     CountStartCallback: (val: number) => void
     CountMaxCallback: (val: number) => void
 }
 
 function Inputer(props: PropsType) {
 
-    let [countStart, setCountStart] = useState<number>(props.countS);
-    let [countMax, setCountMax] = useState<number>(props.countM);
-
-
     const onChangeHandlerStart = (e: ChangeEvent<HTMLInputElement>) => {
-        setCountStart(e.currentTarget.valueAsNumber)
         props.CountStartCallback(e.currentTarget.valueAsNumber)
     }
 
     const onChangeHandlerMax = (e: ChangeEvent<HTMLInputElement>) => {
-        setCountMax(e.currentTarget.valueAsNumber)
         props.CountMaxCallback(e.currentTarget.valueAsNumber)
     }
 
     return (
         <div>
             <div className = {s.Texter}>
-                <span >Start value</span> <input type='number' value={countStart} min={0} max={countMax-1} onChange={onChangeHandlerStart}/>
+                <span >Start value</span> <input type='number' value={props.StartCallback} min={0} max={props.MaxCallback-1} onChange={onChangeHandlerStart}/>
             </div>
             <div className = {s.Texter}>
-                <span >Max value</span> <input type='number' value={countMax} min={countStart+1} max={100} onChange={onChangeHandlerMax}/>
+                <span >Max value</span> <input type='number' value={props.MaxCallback} min={props.StartCallback+1} max={100} onChange={onChangeHandlerMax}/>
             </div>
         </div>
     );
